f737c5385cc33cc7da70f5bd7d270d74f9c59420
Bartosz S <01169625@pw.edu.pl>
2023-04-18 21:43:17 +0200

    removed redundant parameter from exception body response

--- src/main/java/com/example/springauthapi/exceptions/UserAlreadyRegisteredException.java
+++ src/main/java/com/example/springauthapi/exceptions/UserAlreadyRegisteredException.java
@@ -3,8 +3,12 @@ package com.example.springauthapi.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.net.URI;
+
 public class UserAlreadyRegisteredException extends ResponseStatusException {
     public UserAlreadyRegisteredException(String message) {
         super(HttpStatus.BAD_REQUEST, message);
+        // Type is redundant
+        super.setType(URI.create(""));
     }
 }
