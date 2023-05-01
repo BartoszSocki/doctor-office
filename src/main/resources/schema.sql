3e08f94b8a33b47f4c0c4711abf3fc3137d6ae87
Bartosz S <01169625@pw.edu.pl>
2023-05-01 21:44:43 +0200

    changed db schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,46 +1,36 @@
-create table registered_clients(
-    id identity not null,
-    email varchar(255) not null,
-    password varchar(255) not null,
-    primary key (id)
-);
+create table registered_users(
+    pk_id identity not null,
 
-create table registered_doctors(
-    id identity not null,
     email varchar(255) not null,
     password varchar(255) not null,
-    primary key (id)
+
+    name varchar(32) not null,
+    surname varchar(64) not null,
+    gender varchar(1) null,
+    mobile varchar(16) null,
+
+    verified bit default false,
+    primary key (pk_id)
 );
 
 create table doctors_info(
-    id identity not null,
+    pk_id identity not null,
     registered_doctor_id int not null,
 
-    email varchar(255) not null,
-    name varchar(32) not null,
-    surname varchar(32) not null,
-    pesel varchar(20) null,
-    mobile varchar(20) null,
-
-    pwz varchar(100) null,
     specialization varchar(32) not null,
 
-    primary key (id),
-    foreign key (registered_doctor_id) references registered_doctors(id)
+    primary key (pk_id),
+    foreign key (registered_doctor_id) references registered_users(pk_id)
 );
 
 create table clients_info(
-    id identity not null,
+    pk_id identity not null,
     registered_client_id int not null,
 
-    email varchar(255) not null,
-    name varchar(32) not null,
-    surname varchar(32) not null,
-    pesel varchar(20) null,
-    mobile varchar(20) null,
+    date_of_birth date null,
 
-    primary key (id),
-    foreign key (registered_client_id) references registered_clients(id)
+    primary key (pk_id),
+    foreign key (registered_client_id) references registered_users(pk_id)
 );
 
 create table scheduled_visits(
@@ -55,7 +45,7 @@ create table scheduled_visits(
     type varchar(60) not null,
 
     primary key (pk_id),
-    foreign key (fk_registered_doctor_id) references registered_doctors(id)
+    foreign key (fk_registered_doctor_id) references doctors_info(pk_id)
 );
 
 create table planned_visits(
@@ -68,7 +58,7 @@ create table planned_visits(
     cancelled bit default false,
 
     primary key (pk_id),
-    foreign key (fk_registered_doctor_id) references registered_doctors(id),
-    foreign key (fk_registered_client_id) references registered_clients(id),
+    foreign key (fk_registered_doctor_id) references registered_users(pk_id),
+    foreign key (fk_registered_client_id) references registered_users(pk_id),
     foreign key (fk_scheduled_visit_id) references scheduled_visits(pk_id)
 );
